a06d724426211868d5978c7d39886eb3f51f68b8
Jordan Viramontes <jvira007@ucr.edu>
2025-02-03 11:25:50 -0800

    slight changes

--- src/refactorScript.py
+++ src/refactorScript.py
@@ -5,25 +5,15 @@ import yaml
 # CREDIT: I used help from a stack exchange form for setting up astor:
 # https://stackoverflow.com/questions/36022935/rewriting-code-with-ast-python 
 
-filePath = "simple_comparison.py"
-    with open(file_path, "r") as file:
-        example = file.read()  # Save the file's content as a string
+filePath = "src/simple_comparison.py"
+with open(filePath, "r") as file:
+    example = file.read()  # Save the file's content as a string
 
 tree = ast.parse(example)
 
-# Useful classes in Python AST module for analysis and editing:
-# - ast.NodeTransformer
-# - ast.NodeVisitor
-# - ast.Compare
-# - ast.Eq
-# - ast.NotEq
-# - ast.If
-# - ast.dump
-
-# Define a custom NodeVisitor to check for comparison nodes
-
+# for loop that iterates over (walks) the tree
 for node in ast.walk(tree):
-    if isinstance(node, ast.If):
+    if isinstance(node, ast.If): # are we at an if statement node?
         # Bool that tells us if we should switch for later
         shouldSwitch = False
         
@@ -34,36 +24,33 @@ for node in ast.walk(tree):
                 if (isinstance(op, ast.NotEq)):
                     shouldSwitch = True
 
+        # check
+        if (not shouldSwitch): 
+            continue
+
         # check if we have a corresponding else block 
         if node.orelse:
-            if (shouldSwitch):
-                # swap the op (wowie it rhymes!)
-                node.test.ops = [ast.Eq()]
+            # swap the op (wowie it rhymes!)
+            node.test.ops = [ast.Eq()]
 
-                ifLines = []
-                elseLines = []
+            ifLines = []
+            elseLines = []
 
-                # get lines of code from each block
-                for statement in node.body:
-                    ifLines.append(statement)
-                for statement in node.orelse:
-                    elseLines.append(statement)
-                
-                # swap lines of code
-                node.body = []
-                node.orelse = []                
-                for statement in ifLines:
-                    node.orelse.append(statement)
-                for statement in elseLines:
-                    node.body.append(statement)
+            # get lines of code from each block
+            for statement in node.body:
+                ifLines.append(statement)
+            for statement in node.orelse:
+                elseLines.append(statement)
+            
+            # swap lines of code
+            node.body = elseLines
+            node.orelse = ifLines         
 
 # YAML dump
-data = {
-    "refactored_py": tree  # Store the AST as a string
-}
+data = { "refactored_py": tree } # Store the AST as a string
 
 # Write the data to a YAML file
-with open("output.yaml", "w") as file:
+with open("output/output.yaml", "w") as file:
     yaml.dump(data, file, default_flow_style=False, indent=2)
 
 
